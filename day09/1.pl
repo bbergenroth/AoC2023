538a28e3b23e2456ed133d2eadbd0aab7c0e53b6
Brandon Bergenroth <bbergenroth@gmail.com>
2023-12-11 08:20:44 -0500

    day 9 (perl)

---
+++ day09/1.pl
@@ -0,0 +1,41 @@
+#!/usr/bin/perl
+
+sub F {
+	my @a= @{$_[0]};
+	my $zeros = 0;
+	my @d;
+	my $x;
+        
+	foreach(@a) {
+		if ($_ != 0) {
+			$zeros = 1; 
+		}	
+	}
+
+	if ($zeros <= 0) {
+		return 0;
+	}
+
+	for my $i (0 .. $#a-1) {
+		$x = $a[$i+1] - $a[$i];
+		push(@d, $x);
+	}
+
+	$diff = F(\@d);
+	return $diff + $a[$#a];
+}
+
+open(DATA, "<input.txt") or die "$!";
+
+$total = 0;
+
+while(<DATA>) {
+	my @a;
+	push(@a, split(' ', $_)); 
+	$f = F(\@a);
+	$total = $total + $f; 
+
+}
+
+print "$total\n";
+
